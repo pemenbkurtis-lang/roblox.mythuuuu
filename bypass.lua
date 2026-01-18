611215cb9f818ede12860621e7589f5864522cae
pemenbkurtis-lang <pemenbkurtis@gmail.com>
2026-01-18 17:59:57 -0600

    Update bypass.lua

--- bypass.lua
+++ bypass.lua
@@ -36,37 +36,27 @@ local function ConvertBypass(Text)
     return table.concat(New, " ")
 end
 
--- Store original functions globally so bot can't bypass
-getgenv().OriginalSendAsync = {}
-
--- Hook all existing TextChannels IMMEDIATELY
+-- Get the TextChannels folder
 local TextChannels = TextChatService:WaitForChild("TextChannels")
 
-local function HookChannel(channel)
-    if channel:IsA("TextChannel") and not getgenv().OriginalSendAsync[channel] then
-        -- Store original
-        getgenv().OriginalSendAsync[channel] = channel.SendAsync
-        
-        -- Replace with bypass version
-        channel.SendAsync = function(self, message, ...)
-            if type(message) == "string" then
-                message = ConvertBypass(message)
-                print("[BYPASS] Converted:", message)
-            end
-            return getgenv().OriginalSendAsync[channel](self, message, ...)
-        end
+-- Wait for RBXGeneral to exist
+local RBXGeneral = TextChannels:WaitForChild("RBXGeneral")
+
+-- HOOK THE ACTUAL SENDASYNC METHOD BEFORE THE BOT LOADS
+local OriginalSendAsync = RBXGeneral.SendAsync
+
+-- Replace it with our bypassed version
+RBXGeneral.SendAsync = function(self, message, ...)
+    if type(message) == "string" then
+        local converted = ConvertBypass(message)
+        print("[BYPASS] Original:", message)
+        print("[BYPASS] Converted:", converted)
+        return OriginalSendAsync(self, converted, ...)
     end
+    return OriginalSendAsync(self, message, ...)
 end
 
--- Hook existing channels
-for _, channel in pairs(TextChannels:GetChildren()) do
-    HookChannel(channel)
-end
-
--- Hook new channels
-TextChannels.ChildAdded:Connect(HookChannel)
-
--- ALSO hook metamethod as backup
+-- ALSO hook the metamethod as a backup
 local OldNamecall
 OldNamecall = hookmetamethod(game, "__namecall", function(Self, ...)
     local Args = {...}
@@ -75,34 +65,10 @@ OldNamecall = hookmetamethod(game, "__namecall", function(Self, ...)
     if Method == "SendAsync" and Self:IsA("TextChannel") then
         if type(Args[1]) == "string" then
             Args[1] = ConvertBypass(Args[1])
-            print("[BYPASS META] Converted:", Args[1])
         end
     end
     
     return OldNamecall(Self, unpack(Args))
 end)
 
--- NUCLEAR OPTION: Hook the table itself
-local RBXGeneral = TextChannels:FindFirstChild("RBXGeneral")
-if RBXGeneral then
-    local mt = getrawmetatable(RBXGeneral)
-    setreadonly(mt, false)
-    
-    local old = mt.__index
-    mt.__index = newcclosure(function(t, k)
-        if k == "SendAsync" and t:IsA("TextChannel") then
-            return function(self, msg, ...)
-                if type(msg) == "string" then
-                    msg = ConvertBypass(msg)
-                    print("[BYPASS INDEX] Converted:", msg)
-                end
-                return getgenv().OriginalSendAsync[self](self, msg, ...)
-            end
-        end
-        return old(t, k)
-    end)
-    
-    setreadonly(mt, true)
-end
-
-print("[BYPASS] All hooks loaded - messages will be converted")
+print("[BYPASS] Loaded successfully - All messages will be bypassed")
